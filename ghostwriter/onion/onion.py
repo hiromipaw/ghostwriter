6dd4807f0d5df060bf983ef363f3b57e29b5842b
hiro <hiro@torproject.org>
2019-11-13 23:45:56 +0100

    Fix import in onion class

--- ghostwriter/onion/onion.py
+++ ghostwriter/onion/onion.py
@@ -17,8 +17,10 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+from subprocess import Popen
 
-import os, shutil, docker, time
+import logging
+import sys
 
 class Onion:
     """
